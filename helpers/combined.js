ce349dcda00322ada0c72482d8550eadbc0386a8
albertagung <albiardtya@gmail.com>
2018-01-12 21:04:13 +0700

    done tapi harus pake que dibatasin load nya

--- helpers/combined.js
+++ helpers/combined.js
@@ -1,32 +1,47 @@
 const listArticles = require('./listArticles')
 const rssScrape = require('./rssCrape')
 
-const getCombined = (req, res) => {
+const getCombined = async (req, res) => {
   Promise.all([rssScrape.getContent(), listArticles.getListMedium()]).then((data) => {
     let scrapeContent = data[0]
     let scrapeList = data[1]
+    let metadataContent = []
     let metadata = []
     scrapeContent.forEach((eachCategoryLoop) => {
       eachCategoryLoop.forEach((eachContent) => {
         let obj = {
-          guid: eachContent.guid
+          guid: eachContent.guid,
+          link: eachContent.link,
+          createdAt: eachContent.createdAt,
+          author: eachContent.author,
+          title: eachContent.title,
+          content: eachContent.content,
+          categories: eachContent.categories,
+          read_time: eachContent.read_time
         }
-        metadata.push(obj)
+        metadataContent.push(obj)
       })
     })
     scrapeList.forEach((eachList, i) => {
-      // if (metadata[i]) {
-      //   metadata.forEach((meta) => {
-      //     if (meta.guid === eachList.postID) {
-      //       console.log(meta.guid);
-      //       meta['postID'] = eachList.postID
-      //     }
-      //   })
-      // }
-      metadata.forEach((meta) => {
-        if (meta.guid === eachList.postID) {
-          meta['postID'] = eachList.postID
-        }
+      // console.log('ini each', eachList);
+      eachList.forEach((perList) => {
+        metadataContent.forEach((meta) => {
+          if (meta.guid === perList.postID) {
+            console.log('masuk siniii');
+            let objList = {
+              postID: perList.postID,
+              thumbnail: `http://${perList.photo}`,
+              link: meta.link,
+              createdAt: meta.createdAt,
+              author: meta.author,
+              title: meta.title,
+              content: meta.content,
+              categories: meta.categories,
+              read_time: perList.times
+            }
+            metadata.push(objList)
+          }
+        })
       })
     })
     res.send(metadata)
--- helpers/rssCrape.js
+++ helpers/rssCrape.js
@@ -1,41 +1,61 @@
 const parser = require('parse-rss')
 const readingTime = require('reading-time');
+const request = require('request');
+const cheerio = require('cheerio');
 
-let dataArr = ['javascript', 'technology']
+let dataArr = []
+
+const getCategory = (req, res) => {
+  return new Promise ((resolve, reject) => {
+    request('https://medium.com/topics', (error, response, html) => {
+      if (!error && response.statusCode == 200) {
+        var $ = cheerio.load(html);
+        var Preferences = []
+        $('.link.link--noUnderline.u-baseColor--link.u-flex1.u-uiDisplayBold.u-fontSize20').each((i, element) => {
+          var topic_name = element.children[0].data
+          dataArr.push(topic_name.toLowerCase())
+        });
+        resolve(dataArr)
+      }
+    })
+  })
+}
 
 let getContent = (req, res) => {
-  return new Promise((resolve, reject) => {
-    let bigData = []
-    dataArr.forEach( async (newData, i) => {
-      await parser(`https://medium.com/feed/topic/${newData}`, (err, rss) => {
-        if(err){
-          console.log(err);
-        } else {
-          let data =[]
-          rss.forEach((dataRss) => {
-            let stats = readingTime(dataRss.description)
-            // Maniuplating Guid
-            let guidSplitted = dataRss.guid.split('/')
-            let guidId = guidSplitted[4]
-            let obj = {
-              newData: newData,
-              link: dataRss.link,
-              guid: guidId,
-              createdAt: dataRss.date,
-              author: dataRss.author,
-              title: dataRss.title,
-              content: dataRss.description,
-              categories: dataRss.categories,
-              read_time: stats.text
-            }
-            data.push(obj)
-          })
-          debugger
-          bigData.push(data)
-        }
-        if (bigData.length === dataArr.length) {
+  return new Promise ((resolve, reject) => {
+    getCategory().then((dataCategory) => {
+      let bigData = []
+      dataCategory.forEach( async (newData, i) => {
+        await parser(`https://medium.com/feed/topic/${newData}`, (err, rss) => {
+          if(err){
+            console.log(err);
+          } else {
+            let data =[]
+            rss.forEach((dataRss) => {
+              let stats = readingTime(dataRss.description)
+              // Maniuplating Guid
+              let guidSplitted = dataRss.guid.split('/')
+              let guidId = guidSplitted[4]
+              let obj = {
+                newData: newData,
+                link: dataRss.link,
+                guid: guidId,
+                createdAt: dataRss.date,
+                author: dataRss.author,
+                title: dataRss.title,
+                content: dataRss.description,
+                categories: dataRss.categories,
+                read_time: stats.text
+              }
+              data.push(obj)
+            })
+            bigData.push(data)
+          }
+          // if (bigData.length === dataCategory.length) {
+          //   resolve(bigData)
+          // }
           resolve(bigData)
-        }
+        })
       })
     })
   })
