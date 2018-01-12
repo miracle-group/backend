36b7a410f3fd8e1666e1077c75817e0004d5ad25
albertagung <albiardtya@gmail.com>
2018-01-12 15:06:33 +0700

    udah bener

--- routes/rssScrape.js
+++ routes/rssScrape.js
@@ -5,16 +5,13 @@ const readingTime = require('reading-time');
 let dataArr = ['javascript', 'technology']
 
 router.get('/', (req, res) => {
-  let terakhir = ''
-
-  dataArr.forEach( async(newData, i) => {
-
+  let bigData = []
+  dataArr.forEach( async (newData, i) => {
     await parser(`https://medium.com/feed/topic/${newData}`, (err, rss) => {
-      let bigData = []
       if(err){
         console.log(err);
       } else {
-        // console.log(newData, 'DATA');
+        console.log(newData, 'DATA');
         let data =[]
         rss.forEach((dataRss) => {
           let stats = readingTime(dataRss.description)
@@ -23,35 +20,25 @@ router.get('/', (req, res) => {
           let guidId = guidSplitted[4]
           let obj = {
             newData: newData,
-            // link: dataRss.link,
-            // guid: guidId,
-            // createdAt: dataRss.date,
-            // author: dataRss.author,
-            title: dataRss.title
-            // content: dataRss.description,
-            // categories: dataRss.categories,
-            // read_time: stats.text
+            link: dataRss.link,
+            guid: guidId,
+            createdAt: dataRss.date,
+            author: dataRss.author,
+            title: dataRss.title,
+            content: dataRss.description,
+            categories: dataRss.categories,
+            read_time: stats.text
           }
           data.push(obj)
         })
-        console.log(data);
-        bigData.push(data)
-        // bigData = data
-        // res.send(data)
-        console.log()
         debugger
+        bigData.push(data)
       }
-      debugger
+      if (bigData.length === dataArr.length) {
         res.send(bigData)
+      }
     })
-
-    terakhir = bigData
   })
-  console.log(terakhir);
-    // let url   = `https://medium.com/feed/topic/${newData}`
-    // ambilData(){
-    //
-    // }
 })
 
 module.exports = router;
