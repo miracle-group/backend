068f5e7a5afa8e1047a5b765357c01cd632d423e
albertagung <albiardtya@gmail.com>
2018-01-12 14:43:38 +0700

    tolong di debug

--- routes/rssScrape.js
+++ routes/rssScrape.js
@@ -1,35 +1,57 @@
 const router = require('express').Router()
 const parser = require('parse-rss')
-const url    = "https://medium.com/feed/topic/technology"
 const readingTime = require('reading-time');
 
-router.get('/', function(req, res) {
-  parser(url, (err, rss) => {
-    if(err){
-      console.log(err);
-    } else {
-      let data = []
-      rss.forEach((dataRss) => {
-        let stats = readingTime(dataRss.description)
-        console.log(stats);
-        // Maniuplating Guid
-        let guidSplitted = dataRss.guid.split('/')
-        let guidId = guidSplitted[4]
-        let obj = {
-          link: dataRss.link,
-          guid: guidId,
-          createdAt: dataRss.date,
-          author: dataRss.author,
-          title: dataRss.title,
-          content: dataRss.description,
-          categories: dataRss.categories,
-          read_time: stats.text
-        }
-        data.push(obj)
-      })
-      res.send(data)
-    }
+let dataArr = ['javascript', 'technology']
+
+router.get('/', (req, res) => {
+  let terakhir = ''
+
+  dataArr.forEach( async(newData, i) => {
+
+    await parser(`https://medium.com/feed/topic/${newData}`, (err, rss) => {
+      let bigData = []
+      if(err){
+        console.log(err);
+      } else {
+        // console.log(newData, 'DATA');
+        let data =[]
+        rss.forEach((dataRss) => {
+          let stats = readingTime(dataRss.description)
+          // Maniuplating Guid
+          let guidSplitted = dataRss.guid.split('/')
+          let guidId = guidSplitted[4]
+          let obj = {
+            newData: newData,
+            // link: dataRss.link,
+            // guid: guidId,
+            // createdAt: dataRss.date,
+            // author: dataRss.author,
+            title: dataRss.title
+            // content: dataRss.description,
+            // categories: dataRss.categories,
+            // read_time: stats.text
+          }
+          data.push(obj)
+        })
+        console.log(data);
+        bigData.push(data)
+        // bigData = data
+        // res.send(data)
+        console.log()
+        debugger
+      }
+      debugger
+        res.send(bigData)
+    })
+
+    terakhir = bigData
   })
+  console.log(terakhir);
+    // let url   = `https://medium.com/feed/topic/${newData}`
+    // ambilData(){
+    //
+    // }
 })
 
 module.exports = router;
