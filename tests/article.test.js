d5dc007e645a541d5a0b495b37c92598465d243f
Hary Nugraha Putra <harynugrahaputra@gmail.com>
2018-01-16 11:42:58 +0700

    Testing 21 Done

--- tests/article.test.js
+++ tests/article.test.js
@@ -1,7 +1,7 @@
 const tester = require('graphql-tester').tester;
 const request = require('supertest');
 const app = require('../app');
-
+const Conjuction = require('../controllers/controlArticle');
 
 
 describe('GET /api/category/all', () => {
@@ -43,4 +43,61 @@ describe('GET /api/article/all/:userid', () => {
     });
   })
 
+describe('POST new Article', () => {
+const self = this;
+beforeAll(() => {
+  self.test = tester({
+    url: 'http://repod.ga:8000/graphql', contentType: 'application/json'
+  });
+});
+it('Create New Post Should Return Status Code 200',async () => {
+    await self.test(JSON.stringify({
+      query: `mutation{addArticle(input:{
+        title : "Judul",
+        tags : ["Hello","World"],
+        read_time : 10,
+        content : "Lorem Ipsum"
+      }){
+        _id title tags read_time content
+      }}`
+    })).then(response => {
+      expect(response.status).toBe(200);
+    }).catch(err => {
+      expect(err).toBe(null);
+    });
+  });
+
+  it('Read Article Should Status Code 200',async () => {
+    await self.test(JSON.stringify({
+      query: `query{article{
+        _id title read_time preview content tags
+      }}`
+    })).then(response => {
+      expect(response.status).toBe(200);
+    }).catch(err => {
+      expect(err).toBe(false);
+    });
+  });
+})
+
+describe('Update Read Status', () => {
+  test('Check Update Read Status True', (done) => {
+   request(app)
+    .post(`/api/article/6b95fc36159d/true`)
+    .end((err,res) => {
+      expect(res.statusCode).toEqual(200)
+      done()
+    })
+  });
+})
+
+describe('Update Read Status False', () => {
+  test('Check Update Read Status False', (done) => {
+   request(app)
+    .post(`/api/article/6b95fc36159d/false`)
+    .end((err,res) => {
+      expect(res.statusCode).toEqual(200)
+      done()
+    })
+  });
 })
