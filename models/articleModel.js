bece941be400855a9f6352f22625ef8f63f217ca
Hary Nugraha Putra <harynugrahaputra@gmail.com>
2018-01-16 14:15:04 +0700

    TDD Backend Done

--- models/articleModel.js
+++ models/articleModel.js
@@ -2,12 +2,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const articleSchema = new Schema({
-  // title: String,
-  // tags: Array,
-  // read_time: Number,
-  // preview : String,
-  // content: String
-  //
+
   postID: String,
   thumbnail: String,
   createdAt: {type: Date, default: Date.now },
